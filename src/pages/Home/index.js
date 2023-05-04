bfb3a480efd0c61810ac9b39ee76fa38d2c7512a
Diego Fagundes <94011841+DiegoSilva1919@users.noreply.github.com>
2023-05-04 16:41:28 -0300

    Update index.js

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -40,8 +40,8 @@ function App() {
       <Container2>
         <H1>Olá</H1>
 
-        <InputLabel>Nomee</InputLabel>
-        <Input ref={inputName} placeholder="Nomee"></Input>
+        <InputLabel>Nome</InputLabel>
+        <Input ref={inputName} placeholder="Nome"></Input>
 
         <InputLabel>Idade</InputLabel>
         <Input ref={inputAge} placeholder="Idade"></Input>
