7d9193de9d5410cbb1472278f1dc00af8b87e2de
Diego Fagundes <fagundesdiego2015bolcombr@gmail.com>
2024-09-27 15:48:01 -0300

    fix: update baseURL in api.js from localhost to Vercel app URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-	baseURL: "http://localhost:3001",
-	// baseURL: 'https://code-club-first-project-node.vercel.app'
+	// baseURL: "http://localhost:3001"
+	baseURL: "https://user-list-api-seven.vercel.app/"
 });
 
 export default api;
