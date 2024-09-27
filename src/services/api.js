088cc78ca182775c6c19f4fc5b53859801b5b929
Diego Fagundes <fagundesdiego2015bolcombr@gmail.com>
2024-09-27 18:18:59 -0300

    fix: update baseURL in api.js to new Vercel app URL
    - Changed baseURL in api.js from https://user-list-api-seven.vercel.app/ to https://user-list-api-two.vercel.app for correct API endpoint.

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
 	// baseURL: "http://localhost:3001"
-	baseURL: "https://user-list-api-seven.vercel.app/"
+	baseURL: "https://user-list-api-two.vercel.app"
 });
 
 export default api;
